d225dfa6fcb01b7dce9691905ad145e51a2c6da3
Daumantas Jokubaitis <alecoring@gmail.com>
2023-01-11 00:35:30 +0200

    inventory model tests

--- market/tests/fixtures.py
+++ market/tests/fixtures.py
@@ -20,3 +20,6 @@ def django_fixture_setup(django_db_blocker, django_db_setup):
         call_command("loaddata", "db_admin_fixture.json")
         call_command("loaddata", "db_category_fixture.json")
         call_command("loaddata", "db_product_fixture.json")
+        call_command("loaddata", "db_type_fixture.json")
+        call_command("loaddata", "db_brand_fixture.json")
+        call_command("loaddata", "db_product_inventory_fixture.json")
